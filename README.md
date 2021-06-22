95c6ae3ea8893c799fda7d1e9596e608446a5711
00010270 <wiut00010270@gmail.com>
2021-06-22 14:34:50 +0500

    project added

---
+++ README.md
@@ -0,0 +1,35 @@
+# Editor Platform 
+
+### In Editor Platform, You create posts and let the world explore your ideas through your posts. it is the platform for writers and bloggers
+
+You can find Installation Guide below: 
+
+1. First clone application repository 
+```bash
+git clone https://github.com/00010270/Web-Tech-CW2.git
+```
+
+2. Install the necassary dependencies
+```bash
+cd [root folders]/Web-Tech-CW2
+npm install express pug
+```
+
+3. Run the application
+```bash
+node app.js
+```  
+
+### Editor Platform application dependicies 
+- express.js
+- pug.js
+
+
+### Editor Platform application repository link on github.com
+[LINK to github repo](https://github.com/00010270/Web-Tech-CW2/tree/master)   
+
+### Checkout Editor Platform LIVE
+[LINK to Editor Platform website]()
+
+
+   
---
+++ app.js
@@ -0,0 +1,104 @@
+const { urlencoded } = require("express");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.use(express.urlencoded({extended: false}))
+app.set("view engine", "pug");
+
+app.use("/static", express.static("public"));
+
+
+
+
+
+
+app.get("/blogs", (req, res) => {
+    fs.readFile("./data/blogs.json", (err, data) => {
+        if (err) throw err
+
+        const blogs = JSON.parse(data)
+
+        res.render("blogs", {blogs: blogs})
+    })
+
+   
+})
+
+
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/create", (req, res) => {
+    res.render("create")   
+})
+
+
+app.get("/blogs/:id", (req, res) => {
+    const id = req.params.id;
+
+    fs.readFile("./data/blogs.json", (err, data) => {
+        if (err) throw err
+
+        const blogs = JSON.parse(data)
+
+        const blog = blogs.filter(blog => blog.id == id)[0]
+
+        res.render("detail",  {blog: blog} )
+
+    })
+
+})
+
+
+app.post("/create", (req, postRes) => {
+    const title = req.body.title;
+    const blogBody = req.body.blogBody;
+
+    if (title.trim() === "" && blogBody.trim() === "") {
+        postRes.render("create", {error: true})
+    } else {
+        fs.readFile("./data/blogs.json", (err, data) => {
+            if (err) throw err;
+
+            const blogsData = JSON.parse(data);
+
+            blogsData.push({
+                id: id(),
+                title: title,
+                blogBody: blogBody
+            })
+
+            fs.writeFile("./data/blogs.json", JSON.stringify(blogsData), (err) => {
+                if (err) throw err
+
+                postRes.render("create", {success: true})
+            })
+        })
+    }
+})
+
+
+app.get("/api/v1/blogs", (req, res) => {
+    fs.readFile("./data/blogs.json", (err, data) => {
+        if (err) throw err
+
+        const blogs = JSON.parse(data)
+
+        res.json(blogs)
+    })
+})
+
+app.listen(8000, err => {
+    if (err) console.log(err);
+
+    console.log("server is running on port 8000...");
+})
+
+function id () {
+    return "_" + Math.random().toString(36).substr(2, 9);
+}
+
